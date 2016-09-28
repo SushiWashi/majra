127b7cae0d5512b3121d2395d92320c389200a9f
SushiWashi <nazar.abubaker91@gmail.com>
2016-09-28 17:35:52 +0300

    Update

---
+++ blogPost.php
@@ -0,0 +1,67 @@
+<?php include 'header.php';?>
+
+<div class="grid-container">
+    <div class="grid-100">
+        <div class="bigCard post">
+            <div class="blogImage"></div>
+            <h1>Video Games Playing With Imagination</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sodales suscipit nisi id mollis. Cras faucibus neque tortor, et pellentesque massa dapibus quis. Phasellus interdum tellus in augue finibus, vitae hendrerit metus viverra. Aliquam lacinia facilisis lacus. Fusce efficitur cursus lectus, dignissim interdum purus. Curabitur in lectus sed nibh ultrices posuere in sed metus. Sed porta diam a pulvinar pellentesque. Interdum et malesuada fames ac ante ipsum primis in faucibus. Cras vestibulum, sem ut finibus efficitur, libero turpis vestibulum augue, id tincidunt elit lacus cursus orci. Praesent euismod sed massa vitae viverra. Ut imperdiet fringilla sagittis. Maecenas et nisi tempus, efficitur libero a, consequat lacus. Phasellus vehicula vulputate quam eu maximus. Morbi neque magna, faucibus non metus sit amet, dictum vestibulum lacus. Donec nec risus et urna vestibulum consectetur eu nec neque.</p>
+
+            <p>Donec feugiat quam at tortor elementum tempor. Donec suscipit ante nisi, at vulputate risus maximus eget. Nulla dapibus nulla at porttitor pellentesque. Nulla tempus risus ante, eu bibendum orci cursus non. Etiam convallis cursus eleifend. Mauris faucibus dolor at massa fermentum porta. Vivamus at consectetur felis, in tempor justo. Nunc lorem massa, feugiat vel ullamcorper ut, congue at mauris. Mauris placerat urna in leo molestie, sit amet dictum mauris ultrices.</p>
+            
+            <div class="author">
+                
+                <div class="grid-15">
+                    <div class="authorImg" style="background-image:url('https://cdn.tookapic.com/photos/2015/202/a/3/a34ed1f776e866d669460144a72bcca6.jpg?w=1540&q=80&sharp=7&mark=watermark-tileable.png&markfill=1&markalpha=7&s=30193357ceb503d1fd190ce48d1ead4b')"></div>
+                </div>
+                
+                <div class="grid-50">    
+                    <h2>Name Of Author</h2>
+                    
+                    <ul class="social">
+                        <li>
+                            <a href="http://fb.com" target="_blank">
+                            <img src="img/social/Facebook.svg" alt="Facebook"></a>
+                        </li>
+                        <li>
+                            <a href="http://instagram.com" target="_blank">
+                                <img src="img/social/Instagram.svg" alt="Instagram"></a>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://snapchat.com" target="_blank">
+                                <img src="img/social/Snapchat.svg" alt="Snapchat">
+                            </a>
+                        </li>
+                        <li><a href="http://twitter.com" target="_blank">
+                                <img src="img/social/Twitter.svg" alt="Twitter">
+                            </a>
+                        </li>
+                        <li><a href="http://youtube.com" target="_blank">
+                                <img src="img/social/Youtube.svg" alt="YouTube">
+                            </a>
+                        </li>
+                    </ul>
+                    
+                    <p>Donec feugiat quam at tortor elementum tempor. Donec suscipit ante nisi, at vulputate risus maximus eget. Nulla dapibus nulla at porttitor pellentesque. Nulla tempus risus ante, eu bibendum orci cursus non. Etiam convallis cursus eleifend. Mauris faucibus dolor at massa fermentum porta.</p>
+                </div>
+            <div style="clear:both"></div>
+            </div>
+        </div>
+    </div>
+
+<h1>More Learning Support</h1>
+
+<?php for($x=0; $x <=3; $x++) {?>
+<div class="grid-25">
+			<div class="card">
+				<div class="cardImage"></div>
+				<h3><a href="companyProfile.php">Limefish</a></h3>
+				<h1>This will be 2 lines long..just because I can</h1>
+				<h3 class="location">Saar, Kingdom of Bahrain</h3>
+			</div>
+		</div>
+<?php } ?>
+</div>
+
+<?php include 'footer.php';?>
--- resources.php
+++ resources.php
@@ -1,2 +1,51 @@
 <?php include 'header.php'; ?>
+<div class="sort">
+    <div class="grid-container">
+        <div class="grid-50">
+        <form method="POST" action="" style="width:100%">
+            <select name="category">
+                <option value="all">All Categories</option>
+                <option value="all">Comedy</option>
+                <option value="all">Inpsiration</option>
+            </select>
+        
+            <input type="submit">
+        </form>
+        </div>
+        
+        <div class="grid-50">
+            <form method="POST" action="">
+                <input type="input" placeholder="Search.."> 
+            </form>
+        </div>
+    </div>
+</div>
+
+<div class="grid-container">
+<h1 style="margin:1em; text-align:center;">Learning Resources</h1>
+<?php for($x=0; $x <=3; $x++) {?>
+<div class="grid-50">
+			<div class="card supportCard">
+				<div class="cardImage"></div>
+                <div class="grid-100">
+                    <div class="grid-50">
+                        <h3><a href="companyProfile.php">Limefish</a></h3>
+                    </div>
+
+                    <div class="grid-50">
+                        <h3><a href="companyProfile.php">Comedy, Inspiration</a></h3>
+                    </div>
+
+                    <div class="grid-100">
+                    <h1>This will be 2 lines long..just because I can</h1>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla mauris at eros auctor, eu porttitor massa dapibus. In semper in augue in varius. Ut vel semper sem. Pellentesque eleifend ligula ac lacus eleifend, quis suscipit dui euismod. Curabitur condimentum porttitor purus rhoncus posuere. Nullam molestie diam et ex malesuada, quis lacinia ex porttitor. Ut ut interdum nunc. Sed et ullamcorper lacus. Pellentesque sollicitudin sapien vel sapien rhoncus, vitae pretium est varius. Cras nulla justo, euismod et rutrum nec, rutrum a turpis.</p>
+                    </div>
+                </div>
+			</div>
+		</div>
+<?php } ?>
+
+</div>
+
 <?php include 'footer.php'; ?>
