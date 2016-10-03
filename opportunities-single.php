3f7250381ef34390bfaf3bc04b60ecafae1a431b
SushiWashi <nazar.abubaker91@gmail.com>
2016-10-04 00:31:20 +0300

    Added Individual Opportunities Page

---
+++ opportunities-single.php
@@ -0,0 +1,36 @@
+<?php include 'header.php'; ?>
+<div class="crumb">
+	<div class="grid-container">
+		<p style="color:var(--lightGray); float:left;">
+			<a href="/majra/">Home</a> /
+			<a href="opportunities.php">Find Opportunities </a> / 
+			<a href="companyProfile.php">Limefish </a> / 
+			Web Designer
+		</p>
+		
+		<button style="float:right; margin-top:0.6em;">APPLY NOW</button>
+	</div>
+</div>
+
+
+
+<div class="grid-container">
+
+		<div class="card">
+			<div class="grid-100">
+			<h1>Job Title</h1>
+			
+			<h3>Description</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum sollicitudin nisl in feugiat. Proin fermentum interdum nulla, in porttitor diam sagittis vel. Sed et lectus vel elit vestibulum imperdiet sit amet tempus tellus. Nulla vitae nisi nulla. Nunc leo nisi, feugiat quis tristique at, commodo vel urna. Integer euismod lacus ac viverra congue. Curabitur sed semper turpis. Praesent dapibus nibh vel porttitor tempus. Suspendisse ac lacus sit amet lorem varius vestibulum a ut risus. Etiam suscipit orci id quam lacinia posuere. Integer et nisi mollis, dapibus arcu quis, luctus diam.</p>
+			<ul>
+				<li>List 1</li>
+				<li>List 2</li>
+				<li>List 3</li>
+				<li>List 4</li>
+			</ul>
+				
+			</div>
+		</div>
+
+</div>
+<?php include 'footer.php'; ?>
