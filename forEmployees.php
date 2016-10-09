34548bb2d79a2c35cbe3a3322f6b1c07e2063a25
SushiWashi <nazar.abubaker91@gmail.com>
2016-10-10 00:25:58 +0300

    Updates for Employee Page

--- forEmployees.php
+++ forEmployees.php
@@ -10,8 +10,44 @@
                 
                 <div class="grid-100">
                     <ul>
-                        <li><a class="followButton" href="#">SIGN YOUR COMPANY UP</a></li>
-                        <li><a class="outlineButton" href="#">LOG IN</a></li>
+                        <li>
+                            <a class="button" href="#popup2">SIGN YOUR COMPANY UP / LOG IN</a>
+                    <div id="popup2" class="overlay">
+                        <div class="popup">
+                            <div style="clear:both; width:100%; height:20px;">
+                            <a class="close" href="#">&times;</a>
+                                </div>
+                            <div class="content">
+                                <div class="grid-50" style="border-right:1px solid var(--lightGrey)">
+                                    <form method="POST" action="">
+                                        <label>CREATE A NEW ACCOUNT</label><br>
+                                        <input type="text" placeholder="Company"><br>
+                                        <select>
+                                            <option value="industryA">industryA</option>
+                                            <option value="industryB">industryB</option>
+                                            <option value="industryC">industryC</option>
+                                        </select>
+                                        <input type="email" placeholder="Email"><br>
+                                        <input type="password" placeholder="Password"><br>
+                                        <p style="color:var(--lightGray)">By signing up, you agree to our <a href="">terms & conditions</a></p>
+                                        <input type="submit">
+                                    </form>
+                                </div>
+                                
+                                <div class="grid-50">
+                                    <form method="POST" action="">
+                                        <label>LOG IN</label><br>
+                                        <input type="email" placeholder="Email"><br>
+                                        <input type="password" placeholder="Password"><br>
+                                        <a href="#">forgot password?</a>
+                                        <input type="submit">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        
+                        </li>
                     </ul>
                 </div>
                 
@@ -45,16 +81,19 @@
     
     <div class="process">
         <div class="grid-33">
+            <img src="img/process-1.svg">
             <h2>Sign Up</h2>
             <p>Give us your basic company information and get the right subscription package in your inbox! </p>
         </div>
 
         <div class="grid-33">
+            <img src="img/process-2.svg">
             <h2>Profiling</h2>
             <p>We’ll  send  you  a  guide  on  the  type  of  content  you  need!  If  you  can’t  do  it  on  your  own,  don’t worry, we can visit your office and profile the whole place for a simple 50 BHD! (Plus you get to have awesome pictures of your people and office)</p>
         </div>
 
         <div class="grid-33">
+            <img src="img/process-3.svg">
             <h2>Start Hiring</h2>
             <p>Once  we  finish  up  your  profile,  and  you  give  us  the  green  light,  we  publish  it  and  promote  it! Now all you need is to enjoy the process of finding awesome people like you!</p>
         </div>
