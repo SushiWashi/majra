019af4a916fb7c2b7332667e052bcb72180cceef
SushiWashi <nazar.abubaker91@gmail.com>
2016-11-23 14:55:28 +0300

    Added User Profile

---
+++ userProfile.php
@@ -0,0 +1,140 @@
+<?php include 'header.php'; ?>
+
+<?php 
+$is_viewing_profile = true;
+if($is_viewing_profile) {?>
+<div class="is_viewing_profile">
+    <div class="grid-container">
+        <span>You are currently viewing your profile. <a href="#">Edit Profile</a></span>
+    </div>
+</div>
+<?php } ?>
+<div class="companyHeader" style="background-image:url('https://timenerdworld.files.wordpress.com/2012/02/rtr2w75s_comp.jpg?w=720');">
+	<div class="darkBackground">
+		<div class="grid-container">
+			<div class="grid-80 mobile-grid-100">
+                <br class="hide-on-desktop"><br class="hide-on-desktop"><br class="hide-on-desktop">
+
+
+                	<div class="displayPicture hide-on-desktop" style="background-image:url('http://blogs.timesofindia.indiatimes.com/wp-content/uploads/2015/12/mark-zuckerberg.jpg')"></div>
+				<h1 class="mainHeader">Mark Zuckerberg</h1>
+            
+                <ul>
+					<li>Bahrain, BH</li>
+					<li>25 Years Old</li>
+                    <li>BH</li>
+				</ul>
+			</div>
+			
+			<div class="grid-20 hide-on-mobile">
+				<div class="displayPicture" style="background-image:url('http://blogs.timesofindia.indiatimes.com/wp-content/uploads/2015/12/mark-zuckerberg.jpg')"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="grid-container">
+    <div class="grid-30 push-70">
+        <br class="hide-on-desktop">
+        <button class="button orange hide-on-desktop">Request Profile Verification</button>
+        
+        <div class="card orangeCard">
+            <section>
+                
+                <h1>A Quote I Live By</h1>                
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque tempus enim itae dignissim. Cras vulputate, dui eu pharetra egestas, risus felis tristique orci, ac vestibulum urna erat a leo. Curabitur at nibh eleifend, pharetra dui in, tempor risus. </p>
+            </section>
+        </div>
+        
+        <div class="card">
+            <section>
+                <h1>What Can I Add To Your Company?</h1>
+                
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin scelerisque tempus enim vitae dignissim. Cras vulputate, dui eu pharetra egestas, risus felis tristique orci, ac vestibulum urna erat a leo. Curabitur at nibh eleifend, pharetra dui in, tempor risus. </p>
+                
+                <p>Etiam mollis orci id mauris tristique, sed pretium diam feugiat. Sed rhoncus vel urna tincidunt dignissim. Ut lobortis molestie leo. Etiam at justo in nulla tempor maximus. Nunc urna est, sodales sed felis a, euismod condimentum ipsum. Quisque in tortor luctus, volutpat mauris ut, blandit dui. Aliquam non mattis nibh. Maecenas non commodo quam, at vulputate orci. In ultrices commodo lorem vulputate tincidunt.</p>
+            </section>
+        </div>
+        
+        <div class="card">
+            <section>
+                <h1>My Top Considerations Are:</h1>
+                
+                 <ol>
+                    <li>Job Description and Responsibilites</li>
+                    <li>Company Culture and Environment</li>
+                    <li>Salary and Benefits</li>
+                    <li>Company Work and Reputation</li>
+                    <li>Working Conditions (hours, perks)</li>
+                </ol>
+            </section>
+        </div>
+        
+        <button class="button orange hide-on-mobile">Request Profile Verification</button>
+        <br><br>
+    </div>    
+
+    <div class="grid-70 pull-30">
+        <div class="card">
+            <section>
+                <h1>Experience</h1>
+                
+                <div class="grid-50 grid-parent">
+                    <h2>Role</h2>
+                    <h3>Nov 2015 - Nov 2016 (1 year)</h3>
+                    <p>Limefish</p>
+                </div>
+                
+                <div class="grid-50 grid-parent">
+                    <h2>Things I acheived</h2>
+                    <ul class="list">
+                        <li>Created a 10% growth in social media engagement in two months.</li>
+                        <li>Created a 10% growth in social media engagement in two months.</li>
+                        <li>Created a 10% growth in social media engagement in two months.</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+
+        <div class="card">
+            <section>
+                <h1>Education</h1>
+                
+                <div class="grid-50 grid-parent">
+                    <h3 style="color:var(--orange);" class="bold">2002</h3>
+                    <h2>Role</h2>
+                    <h3>Nov 2015 - Nov 2016 (1 year)</h3>
+                    <p>Limefish</p>
+                </div>
+                
+                <div class="grid-50 grid-parent">
+                    <h2>Things I acheived</h2>
+                    <ul class="list">
+                        <li>Job Description and Responsibilites</li>
+                        <li>Company Culture and Environment</li>
+                        <li>Salary and Benefits</li>
+                        <li>Company Work and Reputation</li>
+                        <li>Working Conditions (hours, perks)</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
+
+        <div class="card">
+            <section>
+                <h1>Connect With Me</h1>
+                
+                <dl>
+                    <dt>Phone Number</dt>
+                    <dd>39073986</dd>
+                    <dt>Email</dt>
+                    <dd><a href="mailto:mark@fb.com">mark@fb.com</a></dd>
+                    <dt>Website</dt>
+                    <dd><a href="http://www.fb.com">http://www.fb.com</a></dd>
+                </dl>
+            </section>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
